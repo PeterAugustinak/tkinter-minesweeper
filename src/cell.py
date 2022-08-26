1653b271e2b1518ccb758f838affcf9fa1424546
p-a <peter.augustinak@gmail.com>
2022-08-26 18:54:24 +0200

    Added cell class with button definition with events for left and right click

---
+++ src/cell.py
@@ -0,0 +1,34 @@
+from tkinter import Button
+
+
+class Cell:
+
+    def __init__(self, tag, is_mine=False):
+        self.tag = tag
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+
+
+    def create_btn_object(self, locaton):
+        btn = Button(
+            locaton,
+            text=self.tag,
+        )
+
+        btn.bind(
+            '<Button-1>',  # <Button-1> -> left click
+            self._left_click_actions
+        )
+        btn.bind(
+            '<Button-3>',  # <Button-3> -> right click
+            self._right_click_actions
+        )
+
+        self.cell_btn_object = btn
+
+    def _left_click_actions(self, event):
+        print(f"Button {self.tag} left-clicked! (info: {event}")
+
+
+    def _right_click_actions(self, event):
+        print(f"Button {self.tag} right-clicked! (info: {event}")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 import settings
 import utils
+from cell import Cell
 
 
 # create non-resizable screen with grey background and title
@@ -35,6 +36,21 @@ center_frame = Frame(
     height=utils.height_percent(80),
 )
 center_frame.place(x=utils.width_percent(20), y=utils.height_percent(20))
+
+# demo how grid work
+# c1 = Cell()
+# c1.create_btn_object(center_frame)
+# c1.cell_btn_object.grid(column=0, row=0)
+
+# create grid of cells by looping
+cell_number = 1
+for row in range(settings.GRID_SIZE):
+    for column in range(settings.GRID_SIZE):
+        cell = Cell(
+            str(cell_number if cell_number > 9 else f'0{cell_number}'))
+        cell.create_btn_object(center_frame)
+        cell.cell_btn_object.grid(row=row, column=column)
+        cell_number += 1
 
 # run the window
 root.mainloop()
